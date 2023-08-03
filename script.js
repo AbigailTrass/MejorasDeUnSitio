0a6373a45be61c1e33b9ace41d443b875798097f
lucianomesa <lucianomesa1607@gmail.com>
2023-08-03 09:52:16 -0300

    actualización de archivo script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let foto1 = document.getElementById("img1")
+foto1.addEventListener("click", function(){
+alert("Una foto del planeta")
+})
+let foto2 = document.getElementById("img2")
+foto2.addEventListener("click", function(){
+    alert("Una foto de un servidor")
+})
